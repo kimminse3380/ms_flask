411c26ad1a9cb8c7eeb8f6f2c96cd1b48004bb92
kimminse3380 <mss2214@naver.com>
2021-12-22 15:43:30 +0900

    finsish

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
-from flask import Flask
+import sys
+from flask import Flask, render_template
+from urllib import request
+from bs4 import BeautifulSoup
+
 app = Flask(__name__)
-
-
-@app.route('/')
-def hello():
-    return "Hello flask!"
-
-@app.route('/about')
+ 
+@app.route("/")
+def home():
+    return render_template('index.html', subject="안녕하세요. 반갑습니다. 김민세입니다.")
+#1-1
+@app.route('/<user>')
+def hello(user):
+    return '<h1> hello ' + user
+ 
+#2
+@app.route("/about")
 def about():
-    return "about World!"
+    return render_template('busan1.html', subject="부산중위연령시각화")
+ 
+#3
+@app.route("/show1")
+def show1():
+    return render_template('img_test1.html', image_file='img/1.jpg') 
+      
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
